dad08ad302528747c5bbe921785e61a33cabe1e4
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2019-03-12 18:36:11 -0400

    fixed roll input

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,52 +1,70 @@
 var ObjectID= require('mongodb').ObjectID
 module.exports = function(app,db){
-  app.post('/jobs',(req,res)=>{
-    const job={Company:req.body.Company,Date:req.body.Date}
-    db.collection('jobs').insert(job,(err,res)=>{
+  app.post('/dice',(req,res)=>{
+    let num=Math.floor(Math.random() * 6) + 1
+    const OBJ={roll:num}
+    db.collection('dice').insert(OBJ,(err,results)=>{
       if(err){
         res.send({'error':'An error has occured'})
       }
       else{
-        res.send(result.ops[0])
+        console.log(results.ops[0])
+        console.log("posted!")
+        res.send(results.ops[0])
       }
     })
   })
 
-  app.put('/jobs/:id',(req,res)=>{
+  app.put('/dice/:id/:roll',(req,res)=>{
     const id= req.params.id
     const details={'_id':new ObjectID(id)}
-    const job={Company:req.body.Company,Date:req.body.Date}
-    db.collection('jobs').update(details,(err,item)=>{
+    const OBJ={roll:parseInt(req.params.roll)}
+    db.collection('dice').update(details,OBJ,(err,item)=>{
       if(err){
         res.send({'error':'An error has occured'})
       }
       else{
+        console.log("Updated!")
+        res.send("Updated")
+      }
+    })
+  })
+
+  app.get('/dice/:id',(req,res)=>{
+    const id= req.params.id
+    const details={'_id':new ObjectID(id)}
+    db.collection('dice').findOne(details,(err,item)=>{
+      if(err){
+        res.send({'error':'An error has occured'})
+      }
+      else{
+        console.log("Got!")
         res.send(item)
       }
     })
   })
 
-  app.get('/jobs/:id',(req,res)=>{
-    const id= req.params.id
-    const details={'_id':new ObjectID(id)}
-    db.collection('jobs').findOne(details,(err,item)=>{
+  app.get('/dice',(req,res)=>{
+    db.collection('dice').find().toArray((err,item)=>{
       if(err){
         res.send({'error':'An error has occured'})
       }
       else{
+        console.log(item)
         res.send(item)
       }
     })
   })
 
-  app.delete('/jobs/:id',(req,res)=>{
+  app.delete('/dice/:id',(req,res)=>{
     const id= req.params.id
     const details={'_id':new ObjectID(id)}
-    db.collection('jobs').remove(details,(err,item)=>{
+    db.collection('dice').remove(details,(err,item)=>{
       if(err){
         res.send({'error':'An error has occured'})
       }
       else{
+        console.log("Deleted")
         res.send('Deleted')
       }
     })
--- server.js
+++ server.js
@@ -4,19 +4,18 @@ const bodyParser = require('body-parser');
 const app = express()
 const db = require('./config/db')
 const port = 3000;
+var cors = require('cors')
 
+app.use(cors())
 
 app.use(bodyParser.urlencoded({extended:true}))
 
 
 
- MongoClient.connect(db.url,{useNewUrlParser:true})
-
-
-
-
-
- require('./app/routes')(app,db)
- app.listen(port, ()=>{
-   console.log("live on "+port)
+ MongoClient.connect(db.url,(err,database)=>{
+   if(err) return console.log(err)
+   require('./app/routes')(app,database)
+   app.listen(port, ()=>{
+     console.log("live on http://localhost:"+port)
+   })
  })
